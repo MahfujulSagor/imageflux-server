8ff6bfbdb90d64e940215183755ac559b47df695
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-10-06 17:45:02 +0600

    add cloudinary config in deleteFile

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -1,5 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary';
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 export const deleteFile = async (req, res) => {
   const { public_id } = req.body;
 
