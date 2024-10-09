b10f2f14b7a0ce8984c40b9bca106cd5a5afb56d
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-10-09 12:24:20 +0600

    add userId and change package from appwrite to node-appwrite

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -12,16 +12,17 @@ const account = new Account(client);
 export const authenticate = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const sessionId = authHeader ? authHeader.replace('Bearer ', '') : null;
+  const userId = req.headers['x-user-id'];
   
-  if (!sessionId) {
-    return res.status(401).json({ message: 'Unauthorized' });
+  if (!sessionId || !userId) {
+    return res.status(401).json({ message: 'Unauthorized, sessionId or userId missing' });
   }
 
   try {
     const session = await account.getSession(sessionId);
 
     if (!session) {
-      return res.status(401).json({ message: 'Invalid session' });
+      return res.status(401).json({ message: 'Invalid or expired session' });
     }
 
     req.user = session;
